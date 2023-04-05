60cbf9e9b73bb0860c465ee8513f5d69cec0118a
Umut <umutaygor38@gmail.com>
2023-04-05 07:13:11 +0300

    Eager Loading for List of Products with Categories

--- DataAccessLayer/EntityFramework/EfProductRepository.cs
+++ DataAccessLayer/EntityFramework/EfProductRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,14 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-   public class EfProductRepository: GenericRepository<Product>, IProductDal
+    public class EfProductRepository : GenericRepository<Product>, IProductDal
     {
+        public List<Product> GetProductListWithCategory()
+        {
+            using (var context = new Context())
+            {
+                return context.Products.Include(x => x.Category).ToList();
+            }
+        }
     }
 }
