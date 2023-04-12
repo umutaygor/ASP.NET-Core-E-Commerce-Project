d64342c84b849e7c16bdcf474c1b04bbf21b3328
Umut <umutaygor38@gmail.com>
2023-04-12 23:30:38 +0300

    User can add comments now

--- CoreApplication/Controllers/CommentController.cs
+++ CoreApplication/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,8 +16,19 @@ namespace CoreApplication.Controllers
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult PartialAddComment()
         {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult PartialAddComment(Comment comment)
+        {
+            comment.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            comment.CommentStatus = true;
+            comment.ProductID = 4;
+            commentManager.AddComment(comment);
             return PartialView();
         }
         public PartialViewResult CommentListPartialView(int ID)
---
+++ CoreApplication/Views/ViewComponents/Product/OtherProductsFromSupplier.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApplication.Views.ViewComponents.Product
+{
+    public class OtherProductsFromSupplier:ViewComponent
+    {
+        ProductManager productManager = new ProductManager(new EfProductRepository());
+        public IViewComponentResult Invoke(int ID)
+        {
+            var values=productManager.GetProductListBySupplier(1);
+            return View(values);
+        }
+    }
+}
